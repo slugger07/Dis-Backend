9e784270fbcb7193b09d7dc6721222a8c1155a36
arjitm786 <arjitm786@gmail.com>
2019-11-18 11:36:54 +0530

    Added Request Form: AddBookForm.java

---
+++ administration/src/main/java/sgsits/cse/dis/administration/request/AddBookForm.java
@@ -0,0 +1,77 @@
+package sgsits.cse.dis.administration.request;
+
+import javax.validation.constraints.NotBlank;
+
+public class AddBookForm {
+	
+	private String isbn;
+	
+	@NotBlank
+	private String subjectCategory;
+	
+	private String authorName;
+	
+	private String yearOfPublication;
+	
+	private String edition;
+	
+	private String publisherAndPlace;
+	
+	private long noOfPages;
+	
+	private long price;
+	
+	private String purchaseDate;
+	
+	@NotBlank
+	private String title;
+
+	private String remarks;
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public String getSubjectCategory() {
+		return subjectCategory;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public String getYearOfPublication() {
+		return yearOfPublication;
+	}
+
+	public String getEdition() {
+		return edition;
+	}
+
+	public String getPublisherAndPlace() {
+		return publisherAndPlace;
+	}
+
+	public long getNoOfPages() {
+		return noOfPages;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+	public String getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+	
+	
+
+}
