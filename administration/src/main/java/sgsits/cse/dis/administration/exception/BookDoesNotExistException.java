c19f7ac7bf003aab11091ba03e42757ffb4b0250
arjitm786 <arjitm786@gmail.com>
2019-11-18 16:20:14 +0530

    Created: service getAllBooks, LibraryBookRecordsResponse.java

---
+++ administration/src/main/java/sgsits/cse/dis/administration/exception/BookDoesNotExistException.java
@@ -0,0 +1,9 @@
+package sgsits.cse.dis.administration.exception;
+
+public class BookDoesNotExistException extends Exception {
+		
+
+		public BookDoesNotExistException(String message) {
+			super(message);
+		}
+}
---
+++ administration/src/main/java/sgsits/cse/dis/administration/response/LibraryBookRecordsResponse.java
@@ -0,0 +1,47 @@
+package sgsits.cse.dis.administration.response;
+
+public class LibraryBookRecordsResponse {
+
+	private String authorName;
+	
+	private String edition;
+	
+	private String title;
+	
+	private String status;
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getEdition() {
+		return edition;
+	}
+
+	public void setEdition(String edition) {
+		this.edition = edition;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	
+}
