cfbe560278b44c1a2346752ff1234ec79042aa18
arjitm786 <arjitm786@gmail.com>
2019-11-18 14:59:21 +0530

    Created new package with RestAPI.java file

---
+++ administration/src/main/java/sgsits/cse/dis/administration/constants/RestAPI.java
@@ -0,0 +1,7 @@
+package sgsits.cse.dis.administration.constants;
+
+public class RestAPI {
+	
+	public static final String ADD_BOOK ="/getBook"; 
+	public static final String GET_ALL_BOOKS ="/getAllBooks"; 
+}
