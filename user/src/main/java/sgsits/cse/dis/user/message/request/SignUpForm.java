94e0cb66b9a07cf9ba5da47eef95e046407732b8
vidhanjhawar <vidhan.jhawar98@gmail.com>
2019-12-14 16:05:25 +0530

    401 authorization error resolved

---
+++ user/src/main/java/sgsits/cse/dis/user/message/request/SignUpForm.java
@@ -0,0 +1,48 @@
+package sgsits.cse.dis.user.message.request;
+
+import java.util.Date;
+
+import javax.validation.constraints.*;
+
+public class SignUpForm {
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(max = 60)
+    @Email
+    private String email;
+
+    @NotBlank
+    private Date dob;
+
+    @NotBlank
+    @Size(max = 10)
+    private long mobileNo;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public long getMobileNo() {
+        return mobileNo;
+    }
+}
