8b399c5f07824d5eb86874ee9d923f86417d7822
arjitm786 <arjitm786@gmail.com>
2020-01-20 12:41:40 +0530

    modified:   gateway/src/main/java/sgsits/cse/dis/gateway/constants/GlobalURI.java 	modified:   gateway/src/main/java/sgsits/cse/dis/gateway/security/WebSecurityConfig.java

--- gateway/src/main/java/sgsits/cse/dis/gateway/constants/GlobalURI.java
+++ gateway/src/main/java/sgsits/cse/dis/gateway/constants/GlobalURI.java
@@ -2,5 +2,6 @@ package sgsits.cse.dis.gateway.constants;
 
 public class GlobalURI {
 	
-	public static final String DIS_FRONTEND ="http://10.1.100.76:4200";
+	public static final String DIS_FRONTEND ="http://localhost:4200";
+	//public static final String DIS_FRONTEND ="http://10.1.100.76:4200";
 }
