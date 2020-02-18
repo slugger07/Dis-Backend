21bae330ffa36f1c9628715c9f4d601fa74a7a50
arjitm786 <arjitm786@gmail.com>
2020-02-18 17:33:28 +0530

    Fixed cors

--- gateway/src/main/java/sgsits/cse/dis/gateway/constants/GlobalURI.java
+++ gateway/src/main/java/sgsits/cse/dis/gateway/constants/GlobalURI.java
@@ -2,7 +2,7 @@ package sgsits.cse.dis.gateway.constants;
 
 public class GlobalURI {
 	
-	public static final String DIS_FRONTEND ="http://10.1.100.76:4200";
-	//public static final String DIS_FRONTEND ="http://localhost:4200";
+//	public static final String DIS_FRONTEND ="http://10.1.100.114:4200";
+	public static final String DIS_FRONTEND ="http://localhost:4200";
 
 }
