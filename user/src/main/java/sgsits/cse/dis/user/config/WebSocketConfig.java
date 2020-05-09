bc03d90773ed2579556df439b2c2ab1be8fd8eeb
jainsrijan <srijanpjain@gmail.com>
2020-05-09 11:49:26 +0530

    cors changes

--- user/src/main/java/sgsits/cse/dis/user/config/WebSocketConfig.java
+++ user/src/main/java/sgsits/cse/dis/user/config/WebSocketConfig.java
@@ -39,6 +39,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint(notificationEndpoint);
-        registry.addEndpoint(notificationEndpoint).withSockJS();
+        registry.addEndpoint(notificationEndpoint)
+        .setAllowedOrigins("*")
+        .withSockJS();
     }
 }
